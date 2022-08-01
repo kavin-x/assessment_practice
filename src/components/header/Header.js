c079e216b8ef551caafe1fadb475d06492925465
kavin-x <kavinkumarab@gamil.com>
2022-08-01 09:41:48 +0530

    created header component for navbar

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <div className='header'>
+        <img src="https://img.freepik.com/free-vector/technological-logo-design_1424-39.jpg?w=740&t=st=1659326425~exp=1659327025~hmac=66d8a9805c808b75dc801288ba6dce348301945fa3d3efb42257760aafdf2cee"/>
+        <div>
+        <a href='#' >Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+        <a href='#'>Blog</a>
+            </div>
+        
+    </div>
+  )
+}
+
+export default Header
